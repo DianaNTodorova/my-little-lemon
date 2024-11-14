35bae1a5b017e5e5af0a18ceb0ff02d6170f6d2a
Diana Todorova <Diana.Todorova@stud.sundsgarden.se>
2024-11-14 11:31:28 +0100

    main

--- my-little-lemon/src/pages/Form.js
+++ my-little-lemon/src/pages/Form.js
@@ -1,4 +1,8 @@
 import SmallSection from '../components/SmallSection';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faAsterisk } from '@fortawesome/free-solid-svg-icons';
+import { faPlateWheat } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './Navbar';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -15,15 +19,19 @@ export default function Form() {
           <SmallSection />
         </section>
       </main>
-      <div class="container text-start">
-        <div class="row justify-content-center">
-          <div class="col col-4 mt-5">
-            <label for="exampleFormControlInput1" class="form-label">
-              First Name:
+      <div className="container text-start">
+        <div className="row justify-content-center">
+          <div className="col col-4 mt-5">
+            <label
+              for="exampleFormControlInput1"
+              className="form-label label-text "
+            >
+              First Name:{' '}
+              <FontAwesomeIcon icon={faAsterisk} className="icon-top-corner" />
             </label>
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">
-                +
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">
+                <FontAwesomeIcon icon={faUser} />
               </span>
               <input
                 type="text"
@@ -32,12 +40,16 @@ export default function Form() {
                 aria-describedby="basic-addon1"
               />
             </div>
-            <label for="exampleFormControlInput1" class="form-label">
-              Last Name:
+            <label
+              for="exampleFormControlInput1"
+              className="form-label label-text"
+            >
+              Last Name:{' '}
+              <FontAwesomeIcon icon={faAsterisk} className="icon-top-corner" />
             </label>
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">
-                +
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">
+                <FontAwesomeIcon icon={faUser} />
               </span>
               <input
                 type="text"
@@ -46,27 +58,45 @@ export default function Form() {
                 aria-describedby="basic-addon1"
               />
             </div>
-            <label for="exampleFormControlInput1" class="form-label">
-              Email address
+            <label
+              for="exampleFormControlInput1"
+              className="form-label label-text"
+            >
+              Email address:{' '}
+              <FontAwesomeIcon icon={faAsterisk} className="icon-top-corner" />
             </label>
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">
                 @
               </span>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="youremail@gmail.com"
                 aria-label="Email"
                 aria-describedby="basic-addon1"
               />
             </div>
-            <div class="mb-3">
-              <label for="exampleFormControlTextarea1" class="form-label">
-                Example textarea
+            <label for="exampleFormControlTextarea1" className="form-label">
+              Meal preferences:
+            </label>
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect01">
+                <FontAwesomeIcon icon={faPlateWheat} />
+              </label>
+              <select class="form-select" id="inputGroupSelect01">
+                <option selected>Select...</option>
+                <option value="1">Vegetarian</option>
+                <option value="2">Vegan</option>
+                <option value="3">Gluten-free</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label for="exampleFormControlTextarea1" className="form-label">
+                Special requests:
               </label>
               <textarea
-                class="form-control"
+                className="form-control"
                 id="exampleFormControlTextarea1"
                 rows="3"
               ></textarea>
